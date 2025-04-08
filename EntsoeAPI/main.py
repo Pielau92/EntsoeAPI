b12e05dc806208e0f10e5856be9e78794fc7da60
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-04-08 13:43:58 +0200

    Process API requests with for loop

--- EntsoeAPI/main.py
+++ EntsoeAPI/main.py
@@ -1,5 +1,4 @@
-import datetime, sys, os
-import time
+import datetime
 
 import pandas as pd
 
@@ -7,47 +6,54 @@ from classes import DataQuery
 from utils import get_root_dir
 
 query = DataQuery(get_root_dir())
+today = query.date_today  # today's date
 
-# forecast, yesterday
-start = query.date_today - pd.DateOffset(days=1)
-end = query.date_today
-forecast_yesterday = query.get_all_day_ahead_data(start, end)
+# gather request parameters
+params = {
+    ('forecast', 'yesterday'): {
+        'start': today - pd.DateOffset(days=1),
+        'end': today},
+    ('forecast', 'today'): {
+        'start': today,
+        'end': today + pd.DateOffset(days=1)},
+    ('historical', 'yesterday'): {
+        'start': today - pd.DateOffset(days=1),
+        'end': today},
+}
 
-# forecast, today
-start += pd.DateOffset(days=1)
-end += pd.DateOffset(days=1)
-forecast_today = query.get_all_day_ahead_data(start, end)
-
-# forecast, tomorrow
+# add forecast data request for tomorrow, if available
 if datetime.datetime.now().time() < datetime.datetime.strptime(query.day_ahead_deadline, '%H:%M').time():
     print(f'Day ahead prognosis data not available until today {query.day_ahead_deadline}.')
-    forecast_tomorrow = None
 else:
-    start += pd.DateOffset(days=1)
-    end += pd.DateOffset(days=1)
-    forecast_tomorrow = query.get_all_day_ahead_data(start, end)
+    params[('forecast', 'tomorrow')] = {'start': today, 'end': today + pd.DateOffset(days=1)}
 
-# historical, yesterday
-start = query.date_today - pd.DateOffset(days=1)
-end = query.date_today
-historical_yesterday = query.get_all_historical_data(start, end)
-
-# historical, past years since 2022
+# add historical data requests for past years, since 2022
 years = list(range(2022, datetime.date.today().year + 1))
-historical_years = dict()
 for _year in years:
     start = pd.Timestamp(year=_year, month=1, day=1, hour=0, minute=0, second=0, tz=query.tz)
     end = start + pd.DateOffset(years=1) - pd.Timedelta(hours=1)
-    historical_years[str(_year)] = query.get_all_historical_data(start, end)
+    params[('historical', str(_year))] = {'start': start, 'end': end}
 
-# Export as csv files
-forecast_yesterday.to_csv('../data/forecast_yesterday.csv')
-forecast_today.to_csv('../data/forecast_today.csv')
-historical_yesterday.to_csv('../data/historical_yesterday.csv')
-for key in historical_years:
-    historical_years[key].to_csv(f'../data/historical_{key}.csv')
+# perform API requests and csv export
+for _key in params:
+    req_type: str = _key[0]  # request type (forecast or historical)
+    period: str = _key[1]
+    start: pd.Timestamp = params[_key]['start']
+    end: pd.Timestamp = params[_key]['end']
 
-if forecast_tomorrow:
-    forecast_tomorrow.to_csv('../data/forecast_tomorrow.csv')
+    print(f'Requesting data: {req_type}, {period}.')
+    if req_type == 'forecast':
+        data = query.get_all_day_ahead_data(start, end)
+    elif req_type == 'historical':
+        data = query.get_all_historical_data(start, end)
+    else:
+        print(f'Unknown request type "{req_type}"')
+        continue
 
+    # export to csv
+    export_path = f'../data/{req_type}_{period}.csv'
+    print(f'Exporting to {export_path}')
+    data.to_csv(export_path)
+
+print('done!')
 pass
