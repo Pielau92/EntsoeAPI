29057c59430d99b0d07200fdcf00c47ad9e6f09d
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-11-19 12:59:40 +0100

    Add TimePeriod class
    Introduce TimePeriod class to provide start and end timestamps for:
    - different days relative to a given date
    - a given year
    It should facilitate the use of a registry later on.

---
+++ src/EntsoeAPI/timeperiod.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+class TimePeriod:
+    """Provide start and end timestamps for a day relative to a given date, or for a given year. Timezone information is
+    conserved."""
+
+    def __init__(self, date: pd.Timestamp):
+        self.date = date
+
+    @property
+    def yesterday(self) -> tuple[pd.Timestamp, pd.Timestamp]:
+        start = self.date - pd.DateOffset(days=1)
+        end = self.date
+        return (start, end)
+
+    @property
+    def today(self) -> tuple[pd.Timestamp, pd.Timestamp]:
+        start = self.date
+        end = self.date + pd.DateOffset(days=1)
+        return (start, end)
+
+    @property
+    def tommorow(self) -> tuple[pd.Timestamp, pd.Timestamp]:
+        start = self.date + pd.DateOffset(days=1)
+        end = self.date + pd.DateOffset(days=2)
+        return (start, end)
+
+    def year(self, year: int) -> tuple[pd.Timestamp, pd.Timestamp]:
+        start = pd.Timestamp(year=year, month=1, day=1, hour=0, minute=0, second=0, tz=self.date.tz)
+        end = start + pd.DateOffset(years=1)
+        return (start, end)
