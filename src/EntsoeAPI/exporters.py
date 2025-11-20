cb6907e0b011bbc036be8c4d8a98738ae4af9fbe
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-11-20 13:29:54 +0100

    Move export logic to exporters.py
    Moved export logic from main.py to own module exporters.py. Also structured it as a registry.

---
+++ src/EntsoeAPI/exporters.py
@@ -0,0 +1,36 @@
+import openpyxl
+
+import pandas as pd
+
+from typing import Callable
+
+type ExportFn = Callable[[pd.DataFrame, str], None]
+
+
+def export_csv(df: pd.DataFrame, path: str) -> None:
+    df.to_csv(path)
+
+
+def export_xlsx(data: pd.DataFrame, path: str) -> None:
+    df_tz_naive = data.copy().tz_localize(None)  # turn into timezone naive dataset
+    with pd.ExcelWriter(path, engine='openpyxl') as writer:
+        df_tz_naive.to_excel(writer)
+
+
+exporters: dict[str, ExportFn] = {
+    'csv': export_csv,
+    'xlsx': export_xlsx,
+}
+
+
+def export_data(data: pd.DataFrame, path: str, format: str) -> None:
+    print(f'Exporting to {path}')
+    exporter = exporters[format]
+    exporter(data, path)
+
+
+def export_xlsx_multisheet(data: dict[str, pd.DataFrame], path: str) -> None:
+    with pd.ExcelWriter(path, engine='openpyxl') as writer:
+        for key, df in data.items():
+            df_tz_naive = df.copy().tz_localize(None)  # turn into timezone naive dataset
+            df_tz_naive.to_excel(writer, sheet_name=str(key))
