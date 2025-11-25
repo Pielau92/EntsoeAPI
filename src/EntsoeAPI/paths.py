fe3d0a84d5af5af207afd605f5aa093d1436922a
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-11-25 13:40:51 +0100

    Add output_dir to paths

--- src/EntsoeAPI/paths.py
+++ src/EntsoeAPI/paths.py
@@ -8,6 +8,11 @@ class Paths:
     root: str  # path to root directory
 
     @property
-    def configs(self, filename: str = 'configs.ini'):
+    def configs(self, filename: str = 'configs.ini') -> str:
         """Path to directory."""
         return os.path.join(self.root, filename)
+
+    @property
+    def output_dir(self) -> str:
+        """Path to output directory."""
+        return os.path.join(self.root, 'data')
